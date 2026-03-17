2d7d55cd53f73c32c65008d4bc1f17ef02f6fb37
Sakshiz-19 <zsakshi90@gmail.com>
2026-03-17 20:22:23 +0530

    Update Assignment 1.py

--- Assignments/Assignment 1.py
+++ Assignments/Assignment 1.py
@@ -1,1 +1,14 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!  This is my first Flask code"
+
+@app.route("/greet/<name>")
+def greet(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
